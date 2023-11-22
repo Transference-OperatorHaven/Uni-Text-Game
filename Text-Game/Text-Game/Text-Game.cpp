446fcf24ecb1e11b4515d8012d7e7527f1e7c75a
Operator-Haven <leoshelleyyellow8@gmail.com>
2023-11-22 22:47:01 +0000

    changes man

--- Text-Game/Text-Game/Text-Game.cpp
+++ Text-Game/Text-Game/Text-Game.cpp
@@ -15,12 +15,6 @@
 
 using namespace std;
 /*
-
-spacing out the stuff fr
-
-*/
-
-/*
 variable declarations
 */
 string playerName;
@@ -29,7 +23,7 @@ string playerName;
 function declarations
 */
 void Opening();
-void FirstEncounter();
+void Title();
 
 
 
@@ -37,75 +31,46 @@ void FirstEncounter();
 int main()
 {
     ShowWindow(GetConsoleWindow(), SW_MAXIMIZE);
-    system("Color 0C");
+    //0 = Black  | 8 = Gray
+    //1 = Blue   | 9 = Light Blue
+    //2 = Green  | a = Light Green
+    //3 = Aqua   | b = Light Aqua
+    //4 = Red    | c = Light Red
+    //5 = Purple | d = Light Purple
+    //6 = Yellow | e = Light Yellow
+    //7 = White  | f = Bright White
+    system("Color 0b");
     Opening();
 }
 
 void Opening() 
 {
-    cout << "You are cool\n";
-
-    cout << "   )\\.-.     /`-.      )\\.-.     /`-.        .-.   )\\.---.     /`-.    )\\.--.   )\\.---.  \n";
-    cout << " ,' ,-,_)  ,' _  \\   ,'     )  ,' _  \\   ,'  /  ) (   ,-._(  ,' _  \\  (   ._.' (   ,-._( \n";
-    cout << "(  .   _  (  '-' (  (  .-, (  (  '-' (  (  ) | (   \\  '-,   (  '-' (   `-.`.    \\  '-,   \n";
-    cout << " ) '..' )  )   _  )  ) '._\\ )  )   _  )  ) './ /    ) ,-`    ) ,_ .'  ,_ (  \\    ) ,-`   \n";
-    cout << "(  ,   (  (  ,' ) \\ (  ,   (  (  ,' ) \\ (  ,  (    (  ``-.  (  ' ) \\ (  '.)  )  (  ``-.  \n";
-    cout << " )/'._.'   )/    )/  )/ ._.'   )/    )/  )/..'      )..-.(   )/   )/  '._,_.'    )..-.(  \n";
-    cout << "                                                                                         \n";
-
-
-    cout << "A game about being a new vampire\n";
-
-    FirstEncounter();
+    Title();
 }
 
-void FirstEncounter()
+void Title()
 {
-    Sleep(250);
-    system("Color C0");
-    Sleep(250);
-    system("Color 0C");
-
-    cout << "A pale man stands above you as you feel heavy limbs splayed across a hard wood floor. \"hey? What's your name new blood?\" \t";
-
-    cin >> playerName;
-
-    cout << "He shakes his head as he pulls a cigaretta case out from his coat pocket, lighting it and huffing in the acrid fumes. \"no it aint, stardust.\" \n";
-
-    Sleep(250);
-    system("Color C0");
-    Sleep(250);
-    system("Color 0C");
-    Sleep(125);
-    system("Color C0");
-    Sleep(75);
-    system("color 0C");
-
-    cout << "You awake on a plush bed. Your head feels like someone threw rocks at it... when you slept? when you died?\nThe line between them was hard to distinguish in the moment. A woman sat at the foot with her hands on her hips. She  \t";
-
-    cin >> playerName;
-
-    cout << "That's not it. That's such a weird name too. Who would be named that?";
-    Sleep(230);
-    cout << "\x1b[2K";
-    cout << "\n";
-
-    Sleep(250);
-    system("Color C7");
-    Sleep(250);
-    system("Color 7C");
-
-    cout << "hey? Do you remember your name? \t";
-
-    cout << "P";
-    Sleep(500);
-    cout << "u";
-    Sleep(430);
-    cout << "c";
-    Sleep(521);
-    cout << "k";
-    Sleep(487);
-    cout << "\33[2K";
-    cout << "\n";
+    cout << "\n            ('-.  ('-.         (`-.     ('-.      .-') _           .-') _                    ('-.        .-') _ _ .-') _                    ('-.       (`-.          .-') _     ('-.    .-') _             .-') _            ";
+    cout << "\n          _(  OO)( OO ).-.   _(OO  )_ _(  OO)    ( OO ) )         ( OO ) )                  ( OO ).-.   ( OO ) ( (  OO) )                 _(  OO)    _(OO  )_       (  OO) )   ( OO ).-(  OO) )           ( OO ) )           ";
+    cout << "\n ,--.    (,------/ . --. ,--(_/   ,. (,------,--./ ,--,',-.-'),--./ ,--,' ,----.            / . --. ,--./ ,--,' \\     .'_        ,--.    (,------,--(_/   ,. \\,-.-')/     '._  / . --. /     '._,-.-'),--./ ,--,' ,----.     ";
+    cout << "\n |  |.-') |  .---| \\-.  \\\\   \\   /(__/|  .---|   \\ |  |\\|  |OO|   \\ |  |\\'  .-./-')         | \\-.  \\|   \\ |  |\\ ,`'--..._)       |  |.-') |  .---\\   \\   /(__/|  |OO|'--...__) | \\-.  \\|'--...__|  |OO|   \\ |  |\\'  .-./-')  ";
+    cout << "\n |  | OO )|  | .-'-'  |  |\\   \\ /   / |  |   |    \\|  | |  |  |    \\|  | |  |_( O- )      .-'-'  |  |    \\|  | )|  |  \\  '       |  | OO )|  |    \\   \\ /   / |  |  '--.  .--.-'-'  |  '--.  .--|  |  |    \\|  | |  |_( O- ) ";
+    cout << "\n |  |`-' (|  '--\\| |_.'  | \\   '   /,(|  '--.|  .     |/|  |(_|  .     |/|  | .--, \\       \\| |_.'  |  .     |/ |  |   ' |       |  |`-' (|  '--.  \\   '   /, |  |(_/  |  |   \\| |_.'  |  |  |  |  |(_|  .     |/|  | .--, \\ ";
+    cout << "\n(|  '---.'|  .--'|  .-.  |  \\     /__)|  .--'|  |\\    |,|  |_.|  |\\    |(|  | '. (_/        |  .-.  |  |\\    |  |  |   / :      (|  '---.'|  .--'   \\     /__,|  |_.'  |  |    |  .-.  |  |  | ,|  |_.|  |\\    |(|  | '. (_/ ";
+    cout << "\n |      | |  `---|  | |  |   \\   /    |  `---|  | \\   (_|  |  |  | \\   | |  '--'  |         |  | |  |  | \\   |  |  '--'  /       |      | |  `---.   \\   /  (_|  |     |  |    |  | |  |  |  |(_|  |  |  | \\   | |  '--'  |  ";
+    cout << "\n `------' `------`--' `--'    `-'     `------`--'  `--' `--'  `--'  `--'  `------'          `--' `--`--'  `--'  `-------'        `------' `------'    `-'     `--'     `--'    `--' `--'  `--'  `--'  `--'  `--'  `------'   ";
+    cout << "\n\n";
+    cout << "\n                                                                         __     ____ __ __   __   _  _     ___ _  _  __  ____ ____     ___  __  _  _ ____ ";
+    cout << "\n                                                                        / _\\   / ___(  (  ) (  ) ( \\/ )   / __/ )( \\/  \\/ ___(_  _)   / __)/ _\\( \\/ (  __)";
+    cout << "\n                                                                       /    \\  \\___ \\)(/ (_// (_/\\)  /   ( (_ ) __ (  O \\___ \\ )(    ( (_ /    / \\/ \\) _) ";
+    cout << "\n                                                                       \\_/\\_/  (____(__\\____\\____(__/     \\___\\_)(_/\\__/(____/(__)    \\___\\_/\\_\\_)(_(____)";
+    cout << "\n                                                                                                                 _   _     _         _               _      _                 ";
+    cout << "\n                                                                                                     __      __ (_) | |_  | |__     | |__     __ _  | | __ (_)  _ __     __ _ ";
+    cout << "\n                                                                                                     \\ \\ /\\ / / | | | __| | '_ \\    | '_ \\   / _` | | |/ / | | | '_ \\   / _` |";
+    cout << "\n                                                                                                      \\ V  V /  | | | |_  | | | |   | |_) | | (_| | |   <  | | | | | | | (_| |";
+    cout << "\n                                                                                                       \\_/\\_/   |_|  \\__| |_| |_|   |_.__/   \\__,_| |_|\\_\\ |_| |_| |_|  \\__, |";
+    cout << "\n                                                                                                                                                                        |___/ ";
+    cout << "\n\n";
+    cout << "You died";
 
 }
